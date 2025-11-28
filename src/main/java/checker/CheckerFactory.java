1bfd51504f54c33742f150b29994e4545bf61a9f
AhmedAsaad221B <es.ahmed.asaad2023@alexu.edu.eg>
2025-11-28 14:40:54 +0200

    CsvLoader and checkers

--- src/main/java/checker/CheckerFactory.java
+++ src/main/java/checker/CheckerFactory.java
@@ -1,4 +1,13 @@
 package checker;
 
 public class CheckerFactory {
+
+    public static Checker createChecker(String type) {
+        return switch (type.toLowerCase()) {
+            case "row" -> new RowChecker();
+            case "col" -> new ColumnChecker();
+            case "box" -> new BoxChecker();
+            default -> throw new IllegalArgumentException("Unknown checker type: " + type);
+        };
+    }
 }
